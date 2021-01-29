8dee8a66fd03378f471144b8d8fc0b859bea0b2f
Gonçalo Carito <goncalo.carito.ext@siemens.com>
2021-01-29 13:50:52 +0000

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6ebfd9",
+        deep: "#1e4251",
+        secondary: "#f5fafa"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
